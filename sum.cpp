1a6caedadd11603a5219a0a4e366355f7d8a64b3
swayampanda9001 <swayampanda9001@gmail.com>
2023-05-26 12:17:26 +0530

    save all

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=12, b=10, sum=a+b;
+   cout<<"Enter a: ";
+   cin>>a;
+   cout<<"Enter b: ";
+   cin>>b;
+    cout<<"The sum of "<<a<<" & "<<b<<" is "<<sum;
+    return 0;
+}
